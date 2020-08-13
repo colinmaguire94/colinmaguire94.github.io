5e5c6ae83dfa19835301ab666ade51b2869c0cf0
Colin Maguire <colin.maguire.94@gmail.com>
2020-08-13 15:54:12 -0400

    Updates

---
+++ precache-manifest.2970cdb63cc05850ad70580efbcf10fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c30819de80b5dc434f282fa5d0e31b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbc5b062886fa0f23a23",
+    "url": "/static/css/2.ab965100.chunk.css"
+  },
+  {
+    "revision": "bbc5b062886fa0f23a23",
+    "url": "/static/js/2.8464199f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8464199f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92cb19b07d491be62a9",
+    "url": "/static/js/main.658f8bee.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  },
+  {
+    "revision": "88cb39c2b430019fed1e8ef9bbda6307",
+    "url": "/static/media/BallsToTheWall.88cb39c2.webp"
+  },
+  {
+    "revision": "aa965543bdb3ba9411d2ec7e0eeab21f",
+    "url": "/static/media/Roadblock.aa965543.webp"
+  }
+]);
